2680937af4ba885d1f1a70b7f155d201edf483d2
candh <khichihaider@gmail.com>
2017-01-17 21:59:13 -0500

    Error handling

--- genius.js
+++ genius.js
@@ -13,51 +13,61 @@ const rl = readline.createInterface({
 });
 
 let q = process.argv[2];
-if (q == undefined || q == null || q == ""){
-	console.log('\nNo song provided\n'.red.bold);
-	process.exit();
+if (q == undefined || q == null || q == "") {
+    console.log('\nNo song provided\n'.red.bold);
+    process.exit();
 }
+
 function genius() {
     geniusClient.search(q, function(error, results) {
+    	if(error){
+    		console.log(error)
+    	}
         let hits = JSON.parse(results).response.hits;
-        hits.forEach(function(element, index) {
-            console.log(`${index} >>`.green.bold, element.result.full_title)
-        });
+        if (hits.length > 0) {
+            hits.forEach(function(element, index) {
+                console.log(`${index} >>`.green.bold, element.result.full_title)
+            });
 
-        rl.setPrompt("\nEnter track number >>> ".green)
-        rl.prompt();
+            rl.setPrompt("\nEnter track number >>> ".green)
+            rl.prompt();
 
-        rl.on('line', (line) => {
-            line = parseInt(line)
-          
-            if (Array.apply(null, { length: hits.length }).map(Number.call, Number).indexOf(line) === -1) {
-                console.log(`Not a valid track number, Please try again`.red);
-                rl.prompt();
-            } else {
-            	let answer = line;
-                console.log(`\nTrack Selected: ${answer}`);
-                let path = hits[answer].result.path;
-                let title = hits[answer].result.full_title;
-                let url = `https://www.genius.com${path}`;
+            rl.on('line', (line) => {
+                line = parseInt(line)
 
-                jsdom.env(
-                    `https://genius.com${path}`,
-                    function(err, window) {
-                        let output = `${'_'.repeat(title.length + 1)}\n\n${title}\n${url}\n${'_'.repeat(title.length + 1)}`
-                        term.eraseDisplay();
-                        term.bell();
-                        console.log(output.green.bold);
-                        let lyrics = window.document.getElementsByClassName('lyrics')[0].textContent.trim();
+                if (Array.apply(null, { length: hits.length }).map(Number.call, Number).indexOf(line) === -1) {
+                    console.log(`Not a valid track number, Please try again`.red);
+                    rl.prompt();
+                } else {
+                    let answer = line;
+                    console.log(`\nTrack Selected: ${answer}`);
+                    let path = hits[answer].result.path;
+                    let title = hits[answer].result.full_title;
+                    let url = `https://www.genius.com${path}`;
 
-                        console.log(`\n${lyrics}\n`);
-                        process.exit();
-                    }
-                );
-            }
-        }).on('close', () => {
-            console.log('Bye 😘\n');
-            process.exit(0);
-        });
+                    jsdom.env(
+                        `https://genius.com${path}`,
+                        function(err, window) {
+                            let output = `${'_'.repeat(title.length + 1)}\n\n${title}\n${url}\n${'_'.repeat(title.length + 1)}`
+                            term.eraseDisplay();
+                            term.bell();
+                            console.log(output.green.bold);
+                            let lyrics = window.document.getElementsByClassName('lyrics')[0].textContent.trim();
+
+                            console.log(`\n${lyrics}\n`);
+                            process.exit();
+                        }
+                    );
+                }
+            }).on('close', () => {
+                console.log('Bye 😘\n');
+                process.exit(0);
+            });
+        }
+        else {
+        	console.log("\nNo tracks found with this title\n".red.bold);
+        	process.exit();
+        }
     });
 }
 
