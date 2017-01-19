487a9a811fa64fa6588275e1596f244727c96384
candh <khichihaider@gmail.com>
2017-01-19 15:00:52 -0500

    New version: 1.4.0 - See changelog

--- README.md
+++ README.md
@@ -9,16 +9,30 @@ It was just getting tiring looking up lyrics like... okay maybe not that much ma
     
 ## Usage
     $ glyrics "track name"
+and then follow onscreen instructions
 
-![glyrics](http://i.imgur.com/2QC6MH5.gif)
+![glyrics-demo](http://i.imgur.com/vLvr5ft.gif)
 
 ## TODO
 
-* [ ] scrollable thing fix
+* [x] scrollable thing fix
 * [ ] Make a GUI with all the genius.com features like viewing annotations and stuff
 
 ## Contribution 
 You know what to do.. Just do any of the todos or add your own thing. Fork, make a new branch with your feature, submit a pull request!
+
+
+## Changelog
+Added changelog in version 1.4.0, No changelog for older versions, Sorry 😕
+
+**1.4.0**
+
++ Added a scrollable box (easier to read)
++ Removed manual typing of number for selection of songs. Now using key arrows to navigate through the list of tracks
++ Some other minor UI improvements
+
+## Notes
+Only tested on terminal.app on MacOSX 12.10.2, Just too lazy to test on other platforms.. Can someone tell me if it works on linux too?
 
 ## By
 [@candhforlife](https://twitter.com/candhforlife) 
--- genius.js
+++ genius.js
@@ -1,23 +1,21 @@
+// requires and globals
+const spinner = require('ora')({
+    text: "Searching for tracks...",
+    spinner: "arc",
+    color: "cyan"
+}).start();
+// init the spinner
+const blessed = require('blessed');
 const jsdom = require('jsdom');
 const process = require('process');
 const Genius = require("node-genius");
 // default token - you can generate your own at docs.genius.com
 const geniusClient = new Genius('Wd5-iUH0HjDKBDY3PUztzeo-PiG-34FQKY9tkjH4XfLpG798We7M2blo3xGfQLAO');
 const colors = require('colors')
-const term = require('terminal-kit').terminal;
 const readline = require('readline');
-const ora = require('ora');
+const inquirer = require('inquirer');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-var spinner = ora({
-    text: "Searching for tracks...",
-    spinner : "pipe",
-    color: "cyan"
-}).start();
-
+// get the track name
 let q = process.argv[2];
 if (q == undefined || q == null || q == "") {
     spinner.text = "No track provided";
@@ -25,66 +23,107 @@ if (q == undefined || q == null || q == "") {
     process.exit();
 }
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 function genius() {
+
+    // search the track
     geniusClient.search(q, function(error, results) {
         if (error) {
             spinner.text = error;
             spinner.fail();
         }
+
+        // collect the results in the hits array
         let hits = JSON.parse(results).response.hits;
         if (hits.length > 0) {
+            spinner.succeed();
 
+            // new array with just the titles (useful for looking up the index of the song in the hits array)
+            // because in answer of the prompt we're not provided with the index of the song in the hits array
 
-            spinner.succeed()
-
+            arr = [];
             hits.forEach(function(element, index) {
-                console.log(`\n${index} >>`.green.bold, element.result.full_title)
+                // format it a little bit
+                arr.push(`${colors.red.bold('[')}${(element.result.full_title)}${colors.red.bold(']')}`);
             });
 
-            rl.setPrompt("\nEnter track number >>> ".green)
-            rl.prompt();
 
-            rl.on('line', (line) => {
-                line = parseInt(line)
+            // ask the user which track they want the lyrics for
+            inquirer.prompt([{
+                type: 'list',
+                name: 'track',
+                message: 'Select a track >>>',
+                choices: arr,
+                pageSize: arr.length
+            }]).then(function(answers) {
 
-                if (Array.apply(null, { length: hits.length }).map(Number.call, Number).indexOf(line) === -1) {
-                    console.log(`Not a valid track number, Please try again`.red);
-                    rl.prompt();
-                } else {
+                answer = arr.indexOf(answers.track);
+                let path = hits[answer].result.path;
+                let title = hits[answer].result.full_title;
+                let url = `https://www.genius.com${path}`;
 
-                    let answer = line;
-                    console.log(`\nTrack Selected: ${answer}\n`);
-                    let path = hits[answer].result.path;
-                    let title = hits[answer].result.full_title;
-                    let url = `https://www.genius.com${path}`;
+                spinner.clear();
+                spinner.text = "Fetching lyrics..."
+                spinner.start();
 
-                    spinner.clear();
-                    spinner.text = "Fetching lyrics..."
-                    spinner.start();
-
-                    jsdom.env(
-                        `https://genius.com${path}`,
-                        function(err, window) {
-                            if (err) {
-                                console.log(err);
-                            }
-                            spinner.succeed();
-                            let output = `${'_'.repeat(title.length + 1)}\n\n${title}\n${url}\n${'_'.repeat(title.length + 1)}`
-                            term.eraseDisplay();
-                            term.bell();
-                            console.log(output.green.bold);
-                            let lyrics = window.document.getElementsByClassName('lyrics')[0].textContent.trim();
-
-                            console.log(`\n${lyrics}\n`);
-                            process.exit();
+                // look up lyrics from the website
+                jsdom.env(
+                    `https://genius.com${path}`,
+                    function(err, window) {
+                        if (err) {
+                            console.log(err);
                         }
-                    );
-                }
-            }).on('close', () => {
-                console.log('Bye 😘\n');
-                process.exit(0);
+
+                        spinner.succeed();
+
+                        // formatting the output string
+                        let output = `${'_'.repeat(title.length + 1)}\n\n${title}\n${url}\n${'_'.repeat(title.length + 1)}`;
+
+                        // create a new screen
+                        const screen = blessed.screen({
+                            smartCSR: true
+                        });
+
+                        let lyrics = window.document.getElementsByClassName('lyrics')[0].textContent.trim();
+
+                        // create a scrollable box
+                        var box = blessed.box({
+                            width: '85%',
+                            height: '85%',
+                            left: 'center',
+                            top: 'center',
+                            scrollable: true,
+
+                            scrollbar: {
+                                ch: ' ',
+                                inverse: true
+                            },
+                            alwaysScroll: true,
+                            keys: true,
+                            vi: true,
+                            content: `${colors.green.bold(output)}\n\n${lyrics}`
+                        });
+
+                        // Append our box to the screen.
+                        screen.append(box);
+
+                        // Focus our element.
+                        box.focus();
+
+                        // Render the screen.
+                        screen.render();
+
+                        // on screen close
+                        screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+                            return process.exit(0);
+                        });
+                    }
+                );
             });
+
         } else {
+            // when no track with the provided title is found
             spinner.text = "No tracks found with this title\n".red.bold
             spinner.fail();
             process.exit();
