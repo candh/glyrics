b420ed53e85c0b9ea2901bb020a76de318dca118
candh <khichihaider@gmail.com>
2020-11-29 00:01:50 +0500

    updated to 1.7

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 # glyrics
-A terminal thingy to get lyrics of a track from genius.com 
+
+A terminal thingy to get lyrics of a track from genius.com
 
 [![npm](https://img.shields.io/npm/dm/glyrics.svg)](https://www.npmjs.com/package/glyrics)
 [![npm](https://img.shields.io/npm/v/glyrics.svg)](https://www.npmjs.com/package/glyrics)
 [![Twitter Follow](https://img.shields.io/twitter/follow/candhforlife.svg?style=social&label=Follow)](https://twitter.com/candhforlife)
 
 ## Why
+
 It was just getting tiring looking up lyrics like... okay maybe not that much maybe I was just bored and made this to kill time.
 
-## Install 
-    $ [sudo] npm install glyrics -g
-    
+## Install
+
+    $ [sudo] npm i glyrics -g
+
 ## Usage
+
     $ glyrics "track name"
+
 and then follow onscreen instructions
 
 <p align="center"> 
@@ -21,39 +26,52 @@ and then follow onscreen instructions
 
 ## TODO
 
-* [ ] Make a GUI with all the genius.com features like viewing annotations and stuff
+- [ ] Make a GUI with all the genius.com features like viewing annotations and stuff
 
-## Contribution 
+## Contribution
+
 You know what to do.. Just do any of the todos or add your own thing. Fork, make a new branch with your feature, submit a pull request!
 
 ## Changelog
+
 Added changelog in version 1.4.0, No changelog for older versions, Sorry 😕
 
+**1.7**
+
+- Updated jsdom (I was using an ancient version)
+- UserAgent change.
+- Updated requests
+- Ran prettier on entire project
+
 **1.6.1**
-+ Better regex for highlighting
-+ replaced genius() with IIFE
+
+- Better regex for highlighting
+- replaced genius() with IIFE
 
 **1.6.0**
-+ Bracket [] hightlighting for things like [Verse], [Chorus]. Makes it easier to read
-+ Refactoring and stuff
-+ Added LICENSE
+
+- Bracket [] hightlighting for things like [Verse], [Chorus]. Makes it easier to read
+- Refactoring and stuff
+- Added LICENSE
 
 **1.5.0**
 
-+ Not using `node-genius` anymore.
-+ Displays a more user friendly error if something goes wrong. (not dev friendly)
+- Not using `node-genius` anymore.
+- Displays a more user friendly error if something goes wrong. (not dev friendly)
 
 **1.4.0**
 
-+ Added a scrollable box (easier to read)
-+ Removed manual typing of number for selection of songs. Now using key arrows to navigate through the list of tracks
-+ Some other minor UI improvements
+- Added a scrollable box (easier to read)
+- Removed manual typing of number for selection of songs. Now using key arrows to navigate through the list of tracks
+- Some other minor UI improvements
 
 ## Notes
-+ I encourage that you get your own token at [https://genius.com/api-clients](https://genius.com/api-clients)
-+ Both the selection menu and the lyrics box support `j` and `k` keys for movement... Luckily.
-+ Works on Mac, Linux and even Android (Tested on termux). Install by `yarn global add glyrics` on Android.
+
+- I encourage that you get your own token at [https://genius.com/api-clients](https://genius.com/api-clients)
+- Both the selection menu and the lyrics box support `j` and `k` keys for movement... Luckily.
+- Works on Mac, Linux and even Android (Tested on termux). Install by `yarn global add glyrics` on Android.
 
 ## By
-[@candhforlife](https://twitter.com/candhforlife) 
-PS: Please follow me I'm losing followers because I don't tweet that much 😢 
+
+[@candhforlife](https://twitter.com/candhforlife)
+PS: Please follow me I'm losing followers because I don't tweet that much 😢
--- genius.js
+++ genius.js
@@ -1,137 +1,162 @@
 #!/usr/bin/env node
-const blessed = require('blessed')
-const jsdom = require('jsdom')
-const process = require('process')
-const request = require('request')
-const colors = require('colors')
-const inquirer = require('inquirer')
+const blessed = require("blessed");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const process = require("process");
+const request = require("request");
+const colors = require("colors");
+const inquirer = require("inquirer");
 
-const spinner = require('ora')({
-    text: 'Searching for tracks...',
-    spinner: 'arc',
-    color: 'cyan'
-})
+const spinner = require("ora")({
+  text: "Searching for tracks...",
+  spinner: "arc",
+  color: "cyan",
+});
 
 // get the track name
-const query = process.argv[2]
+const query = process.argv[2];
 if (!query) {
-    spinner.text = 'No track provided'
-    console.log('Usage: glyrics \'track name\'')
-    spinner.fail()
-    process.exit(1)
+  spinner.text = "No track provided";
+  console.log("Usage: glyrics 'track name'");
+  spinner.fail();
+  process.exit(1);
 }
 
 const options = {
-    method: 'GET',
-    url: 'https://api.genius.com/search',
-    qs: {
-        q: query,
-        access_token: 'UARllo5N6CLQYVlqFwolyauSlYiyU_07YTg7HGHkWRbimN4GWPJehPP5fzu9lXeO'
-    }
+  method: "GET",
+  url: "https://api.genius.com/search",
+  qs: {
+    q: query,
+    access_token:
+      "UARllo5N6CLQYVlqFwolyauSlYiyU_07YTg7HGHkWRbimN4GWPJehPP5fzu9lXeO",
+  },
 };
 
-(function() {
-    // start the spinner
-    spinner.start()
+(function () {
+  // start the spinner
+  spinner.start();
 
-    // search the track
-    request(options, (error, response, body) => {
-        if (error) {
-            spinner.text = 'Something went wrong 😢'
-            spinner.fail()
-            return
-        }
-        // collect the results in the hits array
-        const hits = JSON.parse(body).response.hits
+  // search the track
+  request(options, (error, response, body) => {
+    if (error) {
+      spinner.text = "Something went wrong 😢";
+      spinner.fail();
+      return;
+    }
+    // collect the results in the hits array
+    const hits = JSON.parse(body).response.hits;
 
-        if (hits.length) {
-            spinner.succeed()
+    if (hits.length) {
+      spinner.succeed();
 
-            // formatting all the results
-            const results = hits.map(element => `${colors.red.bold('[')}${element.result.full_title}${colors.red.bold(']')}`)
+      // formatting all the results
+      const results = hits.map(
+        (element) =>
+          // prettier-ignore
+          `${colors.red.bold("[")}${element.result.full_title}${colors.red.bold("]")}`
+      );
 
-            // ask the user which track they want the lyrics for
-            inquirer.prompt([{
-                type: 'list',
-                name: 'track',
-                message: 'Select a track >>>',
-                choices: results,
-                pageSize: results.length
-            }]).then((answers) => {
-  
-                const answer = results.indexOf(answers.track)
-                const path = hits[answer].result.path
-                const title = hits[answer].result.full_title
-                const url = `https://www.genius.com${path}`
+      // ask the user which track they want the lyrics for
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "track",
+            message: "Select a track >>>",
+            choices: results,
+            pageSize: results.length,
+          },
+        ])
+        .then((answers) => {
+          const answer = results.indexOf(answers.track);
+          const path = hits[answer].result.path;
+          const title = hits[answer].result.full_title;
+          const url = `https://www.genius.com${path}`;
 
-                spinner.clear()
-                spinner.text = 'Fetching lyrics...'
-                spinner.start()
+          spinner.clear();
+          spinner.text = "Fetching lyrics...";
+          spinner.start();
 
-                // look up lyrics from the website
-                jsdom.env(
-                    url,
-                    (err, window) => {
-                        if (err) {
-                            spinner.stop()
-                            console.log(err)
-                        }
+          const resources = new jsdom.ResourceLoader({
+            userAgent:
+              "Dalvik/2.1.0 (Linux; U; Android 7.1.2; AFTA Build/NS6264) CTV",
+          });
 
-                        // stop the spinner
-                        spinner.succeed()
+          JSDOM.fromURL(url, { resources })
+            .then((dom) => {
+              const window = dom.window;
 
-                        // formatting the output header
-                        const output = `${'_'.repeat(title.length + 1)}\n\n${title}\n${url}\n${'_'.repeat(title.length + 1)}`
+              // stop the spinner
+              spinner.succeed();
 
-                        // create a new screen
-                        const screen = blessed.screen({
-                            smartCSR: true
-                        })
+              // formatting the output header
+              // prettier-ignore
+              const output = `${"_".repeat(title.length + 1)}\n\n${title}\n${url}\n${"_".repeat(title.length + 1)}`;
 
-                        let lyrics = window.document.getElementsByClassName('lyrics')[0].textContent.trim().split('\n')
-                        
-                        // highlighting things like [Verse], [Chorus] etc
-                        const pattern = /^\[(.*?)\]$/g
-                        lyrics = lyrics.map(el => el.match(pattern) ? colors.green.bold(el) : el).join('\n')
+              // create a new screen
+              const screen = blessed.screen({
+                smartCSR: true,
+              });
 
-                        // create a scrollable box
-                        const box = blessed.box({
-                            width: '85%',
-                            height: '85%',
-                            left: 'center',
-                            top: 'center',
-                            scrollable: true,
-                            scrollbar: {
-                                ch: ' ',
-                                inverse: true
-                            },
-                            alwaysScroll: true,
-                            keys: true,
-                            vi: true,
-                            content: `${colors.green.bold(output)}\n\n${lyrics}`
-                        })
+              let lyrics;
+              try {
+                lyrics = window.document
+                  .getElementsByClassName("lyrics")[0]
+                  .textContent.trim()
+                  .split("\n");
+              } catch (e) {
+                spinner.text = `Couldn't get lyrics. Sorry!\nPlease visit ${url} in your browser.`.red.bold;
+                spinner.fail();
+                process.exit();
+              }
 
-                        // Append our box to the screen.
-                        screen.append(box)
+              // highlighting things like [Verse], [Chorus] etc
+              const pattern = /^\[(.*?)\]$/g;
+              lyrics = lyrics
+                .map((el) => (el.match(pattern) ? colors.green.bold(el) : el))
+                .join("\n");
 
-                        // Focus our element.
-                        box.focus()
+              // create a scrollable box
+              const box = blessed.box({
+                width: "80%",
+                height: "80%",
+                left: "center",
+                top: "center",
+                scrollable: true,
+                scrollbar: {
+                  ch: " ",
+                  inverse: true,
+                },
+                alwaysScroll: true,
+                keys: true,
+                vi: true,
+                content: `${colors.green.bold(output)}\n\n${lyrics}`,
+              });
 
-                        // Render the screen.
-                        screen.render()
+              // Append our box to the screen.
+              screen.append(box);
 
-                        // on screen close
-                        screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-                            return process.exit(0)
-                        })
-                    }
-                )
+              // Focus our element.
+              box.focus();
+
+              // Render the screen.
+              screen.render();
+
+              // on screen close
+              screen.key(["escape", "q", "C-c"], function (ch, key) {
+                return process.exit(0);
+              });
             })
-        } else {
-            // when no track with the provided title is found
-            spinner.text = 'No tracks found with this title\n'.red.bold
-            spinner.fail()
-            process.exit()
-        }
-    })
+            .catch((err) => {
+              spinner.stop();
+              console.log(err);
+            });
+        });
+    } else {
+      // when no track with the provided title is found
+      spinner.text = "No tracks found with this title\n".red.bold;
+      spinner.fail();
+      process.exit();
+    }
+  });
 })();
